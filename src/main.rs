53fd7aa7a5dadcaa65c4f281b294ae86a4bad87d
viniciusdutra314 <dutravinisousa2@usp.br>
2026-04-04 17:22:39 -0300

    feat: minimal example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+mod midi_reader;
+
 use cpal::{
     StreamConfig,
     traits::{DeviceTrait, HostTrait, StreamTrait},
 };
+use rustysynth::{SoundFont, Synthesizer, SynthesizerSettings};
 
+use std::cmp::Ordering;
 use std::collections::BinaryHeap;
+use std::fs::File;
 use std::iter::Iterator;
-use std::{arch::x86_64::_MM_EXCEPT_OVERFLOW, cmp::Ordering};
+use std::sync::Arc;
+
 #[derive(Debug, Clone, Copy)]
 enum PitchEnum {
     A,
@@ -46,8 +52,8 @@ impl Accidental {
         }
     }
 }
-#[derive(Debug, Clone, Copy)]
 
+#[derive(Debug, Clone, Copy)]
 struct Note {
     pitch: PitchEnum,
     accident: Accidental,
@@ -56,16 +62,17 @@ struct Note {
 
 impl Note {
     fn new(pitch: PitchEnum, accident: Accidental, octave: u8) -> Self {
-        Note {
+        Self {
             pitch,
             accident,
             octave,
         }
     }
-    fn frequency(&self) -> f32 {
+
+    fn midi_note_number(&self) -> u8 {
         let semitone = self.pitch.base_semitone() as i16 + self.accident.semitone_modifier() as i16;
-        let midi_index = ((self.octave as i16 + 1) * 12) + semitone;
-        440.0 * f32::powf(2.0, (midi_index as f32 - 69.0) / 12.0)
+        let midi = ((self.octave as i16 + 1) * 12) + semitone;
+        midi.clamp(0, 127) as u8
     }
 }
 
@@ -105,11 +112,12 @@ struct Scheduler {
 impl Scheduler {
     pub fn new<T: IntoIterator<Item = Event>>(events: T) -> Self {
         Self {
-            queue: BinaryHeap::from_iter(events.into_iter().map(|event| std::cmp::Reverse(event))),
+            queue: BinaryHeap::from_iter(events.into_iter().map(std::cmp::Reverse)),
             current_note: None,
             current_note_start_time: None,
         }
     }
+
     pub fn get_state(&mut self, time: f32) -> Option<(Note, f32)> {
         while let Some(next_event) = self.queue.peek().map(|r| r.0) {
             if time >= next_event.time {
@@ -120,82 +128,108 @@ impl Scheduler {
                 break;
             }
         }
-
         self.current_note
             .map(|n| (n, self.current_note_start_time.unwrap()))
     }
 }
 
-//pub fn wave(time:f32,amplitude:f32,d)
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let host = cpal::default_host();
     let device = host
         .default_output_device()
-        .ok_or("Not output device found")?;
-    let mut supported_configs_range = device.supported_output_configs()?;
-    let config: StreamConfig = supported_configs_range
+        .ok_or("No output device found")?;
+
+    let mut supported_configs = device.supported_output_configs()?;
+    let config: StreamConfig = supported_configs
         .next()
-        .ok_or("")?
-        .with_max_sample_rate()
+        .ok_or("No supported output config")?
+        .with_sample_rate(48000)
         .into();
-    let mut time = 0.0f32;
-    let pi = std::f32::consts::PI;
-    let sample_rate = config.sample_rate as f32;
+
+    let sample_rate_hz = config.sample_rate;
     let channels = config.channels as usize;
+
+    let mut sf2 = File::open("/home/vinicius/github/GuitarTuner/Yamaha_XG_Sound_Set.sf2")?;
+    let sound_font = Arc::new(SoundFont::new(&mut sf2)?);
+    let synth_settings = SynthesizerSettings::new(sample_rate_hz as i32);
+    let mut synth = Synthesizer::new(&sound_font, &synth_settings)?;
 
     let events = vec![
         Event {
             time: 0.0,
-            note: Note::new(PitchEnum::C, Accidental::Natural, 4),
+            note: Note::new(PitchEnum::C, Accidental::Natural, 2),
         },
         Event {
             time: 1.0,
-            note: Note::new(PitchEnum::D, Accidental::Natural, 4),
+            note: Note::new(PitchEnum::D, Accidental::Natural, 2),
         },
         Event {
             time: 2.0,
-            note: Note::new(PitchEnum::E, Accidental::Natural, 4),
+            note: Note::new(PitchEnum::E, Accidental::Natural, 2),
         },
         Event {
             time: 3.0,
-            note: Note::new(PitchEnum::F, Accidental::Natural, 4),
+            note: Note::new(PitchEnum::F, Accidental::Natural, 2),
         },
         Event {
             time: 4.0,
-            note: Note::new(PitchEnum::G, Accidental::Natural, 4),
+            note: Note::new(PitchEnum::G, Accidental::Natural, 2),
         },
         Event {
             time: 5.0,
-            note: Note::new(PitchEnum::A, Accidental::Natural, 4),
+            note: Note::new(PitchEnum::A, Accidental::Natural, 2),
         },
         Event {
             time: 6.0,
-            note: Note::new(PitchEnum::B, Accidental::Natural, 4),
+            note: Note::new(PitchEnum::B, Accidental::Natural, 2),
         },
         Event {
             time: 7.0,
-            note: Note::new(PitchEnum::C, Accidental::Natural, 5),
+            note: Note::new(PitchEnum::C, Accidental::Natural, 3),
         },
     ];
     let mut scheduler = Scheduler::new(events);
+
+    let mut time = 0.0f32;
+    let dt = 1.0f32 / sample_rate_hz as f32;
+    let mut current_midi: Option<u8> = None;
+
+    let mut l = [0.0f32; 1];
+    let mut r = [0.0f32; 1];
+
     let stream = device.build_output_stream(
-        &config.into(),
+        &config,
         move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
             for frame in data.chunks_mut(channels) {
-                let (note, start_time) = scheduler.get_state(time).unwrap();
-                let rel_time = time - start_time;
-                let attack = rel_time;
-                let value = attack
-                    * f32::sin(2.0 * pi * note.frequency() * rel_time)
-                    * f32::exp(-(5.0 * rel_time));
-                for sample in frame.iter_mut() {
-                    *sample = value;
+                if let Some((note, _start)) = scheduler.get_state(time) {
+                    let midi = note.midi_note_number();
+                    if current_midi != Some(midi) {
+                        if let Some(prev) = current_midi {
+                            synth.note_off(0, prev as i32);
+                        }
+                        synth.note_on(0, midi as i32, 100);
+                        current_midi = Some(midi);
+                    }
                 }
-                time += 1.0 / sample_rate;
+
+                synth.render(&mut l, &mut r);
+                let sample_l = l[0];
+                let sample_r = r[0];
+
+                if channels >= 2 {
+                    frame[0] = sample_l;
+                    frame[1] = sample_r;
+                    for sample in frame.iter_mut().skip(2) {
+                        *sample = (sample_l + sample_r) * 0.5;
+                    }
+                } else {
+                    frame[0] = (sample_l + sample_r) * 0.5;
+                }
+
+                time += dt;
             }
         },
-        move |err| {},
+        move |_err| {},
         None,
     )?;
 
@@ -203,5 +237,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         std::thread::park();
     }
-    return Ok(());
 }
